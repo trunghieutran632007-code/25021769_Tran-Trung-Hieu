c904807587dd02ed4984b86610572f03c047b291
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-01-20 09:32:54 +0700

    .

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-from pathlib import Path
-# Write a file (mode 'w' overwrites)
-file_path = "data.txt"
-f = open(file_path, "w", encoding="utf-8")
-f.write("Alice")
-f.close()
-
-# Append a second line
-f = open(file_path, "a", encoding="utf-8")
-f.write("\nBob")
-f.close()
-
-#print(Path(file_path).read_text(encoding="utf-8"))
-with  open(file_path, "a", encoding = "utf-8") as f:
-    f.write("\nabc1234")
-with open(file_path, 'r', encoding="utf-8") as f:
-    print(f.read())
+with open("a.txt", "w") as f:
+   f.write("ABC")
+with open("a.txt", "r+") as f:
+   f.read(2)
+   f.write("X")
